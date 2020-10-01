9d727c07e6522a817c442ac97d84d74b038eaddb
ingvord <mail@ingvord.ru>
2020-10-01 12:14:08 +0200

    progress

--- src/widgets/controller.js
+++ src/widgets/controller.js
@@ -2,23 +2,24 @@ import {WaltzWidget} from "@waltz-controls/middleware";
 import {kPiAxisController, kPiAxisUpdateMotor} from "controllers/pi_controller";
 import {getCrystalId, getMotorIds} from "utils";
 
-function createCrystalPanels(controller) {
+function createCrystalPanels(controller, root) {
     return [1, 2, 3, 4]
         .map(id => id + (controller.id * 4))
         .map(id => ({
             view: "datatable",
             id: `${controller.id}:${id}`,
+            editable: true,
             columns: [
                 {id: "id", header: `Crystal ${id}`, width: 150},
                 {
                     id: "neg", header: "", width: 30, template() {
-                        return `<span class="webix_icon negative mdi mdi-minus"></span>`
+                        return `<div class='webix_el_button negative webix_base'><span class="webix_icon mdi mdi-minus"></span></div>`
                     }
                 },
-                {id: "value", header: "Value", width: 120},
+                {id: "value", header: "Value", width: 120, editor: "text"},
                 {
                     id: "pos", header: "", template() {
-                        return `<span class="webix_icon switcher mdi mdi-plus"></span>`
+                        return `<div class='webix_el_button positive webix_base'><span class="webix_icon mdi mdi-plus"></span></div>`
                     }, width: 30
                 },
                 {
@@ -54,7 +55,38 @@ function createCrystalPanels(controller) {
                     m2: controller.motors.getItem(getMotorIds(`${id}`)[1]).position,
                     m3: controller.motors.getItem(getMotorIds(`${id}`)[2]).position
                 }
-            ]
+            ],
+            onClick: {
+                "negative": (ev, coord) => {
+                    const row = coord.row;
+                    const datatable = $$(`${controller.id}:${id}`);
+                    datatable.editStop();
+                    const value = parseFloat(datatable.getItem(row).value)
+                    switch (row) {
+                        case 'Angle':
+                            return root.decrementAngle(id, value, getMotorIds(`${id}`))
+                        case 'Bias':
+                            return root.leftBias(id, value, getMotorIds(`${id}`))
+                        case 'Focus':
+                            return root.backwardFocus(id, value, getMotorIds(`${id}`))
+                    }
+
+                },
+                "positive": (ev, coord) => {
+                    const row = coord.row;
+                    const datatable = $$(`${controller.id}:${id}`);
+                    datatable.editStop();
+                    const value = parseFloat(datatable.getItem(row).value)
+                    switch (row) {
+                        case 'Angle':
+                            return root.incrementAngle(id, value, getMotorIds(`${id}`))
+                        case 'Bias':
+                            return root.rightBias(id, value, getMotorIds(`${id}`))
+                        case 'Focus':
+                            return root.forwardFocus(id, value, getMotorIds(`${id}`))
+                    }
+                }
+            }
         }))
 }
 
@@ -82,8 +114,218 @@ export default class ControllerWidget extends WaltzWidget {
         return {
             header: `${this.controller.ip}`,
             body: {
-                rows: createCrystalPanels(this.controller)
+                rows: createCrystalPanels(this.controller, this)
             }
         }
     }
+
+    /**
+     * [+][+][-]
+     *
+     * @param value
+     * @param motorIds
+     */
+    decrementAngle(crstlId, value, motorIds) {
+        const controller = this.app.getController(`${this.controller.id}`)
+
+        const motors = controller.controller.motors;
+        const m1 = motors.getItem(motorIds[0]);
+        const m2 = motors.getItem(motorIds[1]);
+        const m3 = motors.getItem(motorIds[2]);
+
+        let values;
+        switch (crstlId) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                values = {
+                    [motorIds[0]]: m1.position + value,
+                    [motorIds[1]]: m2.position + value,
+                    [motorIds[2]]: m3.position - value
+                }
+                break;
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+                values = {
+                    [motorIds[0]]: m1.position - value,
+                    [motorIds[1]]: m2.position - value,
+                    [motorIds[2]]: m3.position + value
+                }
+                break;
+        }
+
+        controller.move(values);
+    }
+
+    /**
+     * [-][-][+]
+     *
+     *
+     * @param value
+     * @param motorIds
+     */
+    incrementAngle(crstlId, value, motorIds) {
+        const controller = this.app.getController(`${this.controller.id}`)
+
+        const motors = controller.controller.motors;
+        const m1 = motors.getItem(motorIds[0]);
+        const m2 = motors.getItem(motorIds[1]);
+        const m3 = motors.getItem(motorIds[2]);
+
+        let values;
+        switch (crstlId) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                values = {
+                    [motorIds[0]]: m1.position - value,
+                    [motorIds[1]]: m2.position - value,
+                    [motorIds[2]]: m3.position + value
+                }
+                break;
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+                values = {
+                    [motorIds[0]]: m1.position + value,
+                    [motorIds[1]]: m2.position + value,
+                    [motorIds[2]]: m3.position - value
+                }
+                break;
+        }
+
+        controller.move(values);
+    }
+
+    /**
+     * move left
+     *
+     * @param crstlId
+     * @param value
+     * @param motorIds
+     */
+    leftBias(crstlId, value, motorIds) {
+        const controller = this.app.getController(`${this.controller.id}`)
+
+        const motors = controller.controller.motors;
+        const m1 = motors.getItem(motorIds[0]);
+        const m2 = motors.getItem(motorIds[1]);
+        const m3 = motors.getItem(motorIds[2]);
+
+        let values;
+        switch (crstlId) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                values = {
+                    [motorIds[0]]: m1.position - value,
+                    [motorIds[1]]: m2.position + value,
+                    [motorIds[2]]: m3.position - value
+                }
+                break;
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+                values = {
+                    [motorIds[0]]: m1.position - value,
+                    [motorIds[1]]: m2.position + value,
+                    [motorIds[2]]: m3.position + value
+                }
+                break;
+        }
+
+        controller.move(values);
+    }
+
+    /**
+     * move right
+     *
+     * @param crstlId
+     * @param value
+     * @param motorIds
+     */
+    rightBias(crstlId, value, motorIds) {
+        const controller = this.app.getController(`${this.controller.id}`)
+
+        const motors = controller.controller.motors;
+        const m1 = motors.getItem(motorIds[0]);
+        const m2 = motors.getItem(motorIds[1]);
+        const m3 = motors.getItem(motorIds[2]);
+
+        let values;
+        switch (crstlId) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                values = {
+                    [motorIds[0]]: m1.position + value,
+                    [motorIds[1]]: m2.position - value,
+                    [motorIds[2]]: m3.position + value
+                }
+                break;
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+                values = {
+                    [motorIds[0]]: m1.position + value,
+                    [motorIds[1]]: m2.position - value,
+                    [motorIds[2]]: m3.position - value
+                }
+                break;
+        }
+
+        controller.move(values);
+    }
+
+    /**
+     *
+     *
+     * @param crstlId
+     * @param value
+     * @param motorIds
+     */
+    backwardFocus(crstlId, value, motorIds) {
+        const controller = this.app.getController(`${this.controller.id}`)
+
+        const motors = controller.controller.motors;
+        const m1 = motors.getItem(motorIds[0]);
+        const m2 = motors.getItem(motorIds[1]);
+        const m3 = motors.getItem(motorIds[2]);
+
+        const values = {
+            [motorIds[0]]: m1.position - value,
+            [motorIds[1]]: m2.position - value,
+            [motorIds[2]]: m3.position - value
+        }
+
+        controller.move(values);
+    }
+
+    forwardFocus(crstlId, value, motorIds) {
+        const controller = this.app.getController(`${this.controller.id}`)
+
+        const motors = controller.controller.motors;
+        const m1 = motors.getItem(motorIds[0]);
+        const m2 = motors.getItem(motorIds[1]);
+        const m3 = motors.getItem(motorIds[2]);
+
+        const values = {
+            [motorIds[0]]: m1.position + value,
+            [motorIds[1]]: m2.position + value,
+            [motorIds[2]]: m3.position + value
+        }
+
+        controller.move(values);
+    }
+
+
 }
